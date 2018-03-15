2d41d7920ace5a83521134792f5fd5ec2b32c77d
JavaScriptErika <erikajritter@gmail.com>
2018-03-15 15:44:25 -0400

    Display images, add temp styling for display

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-console.log("test")
 
+fetch(`https://images-api.nasa.gov/search?q=milkyway&media_type=image`)
+    .then((response) => response.json())
+    .then((response) => {
+        const { items: imagesFromSearchArray } = response.collection
+        displayImagesFromApi(imagesFromSearchArray)
+    })
+
+
+
+    const displayImagesFromApi = (imagesArray) => {
+        const imageContainer = document.querySelector('.image-container')
+
+        const displayImages = imagesArray.map((imgItem) => {
+                const createImage = new Image()
+                const createImageDiv = document.createElement('div')
+
+                createImage.src = imgItem.links[0].href
+                createImageDiv.appendChild(createImage)
+                return imageContainer.appendChild(createImageDiv)
+            })
+    }
