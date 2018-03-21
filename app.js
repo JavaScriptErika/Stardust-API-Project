f3d6cab27c0228d7252bc0cb5dd76667df889c4f
JavaScriptErika <erikajritter@gmail.com>
2018-03-21 17:35:54 -0400

    Add local api storage, display past searches

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const searchButton = document.getElementById('search-btn')
 const searchInputField = document.getElementById('image-search')
+const localWindowStorage = window.localStorage
+const showPreviousSearches = document.querySelector('.show-previous-searches')
 
 searchButton.addEventListener('click', (e) => {
     e.preventDefault
     const userSearchValue = searchInputField.value
 
     userSearchValue === "" ? requestNasaApi() : requestNasaApi(userSearchValue)
+    userSearchValue !== "" ? displayPastSavedSearches(userSearchValue) : ''
 })
 
 
-var requestNasaApi= (SearchValue = 'stars') => {
-    console.log(SearchValue)
+const requestNasaApi= (SearchValue = 'stars') => {
     fetch(`https://images-api.nasa.gov/search?q=${SearchValue}&media_type=image`)
     .then((response) => response.json())
     .then((response) => {
@@ -19,17 +21,21 @@ var requestNasaApi= (SearchValue = 'stars') => {
     })
 }
 
+const displayImagesFromApi = (imagesArray) => {
+    const imageContainer = document.querySelector('.image-container')
 
+    const displayImages = imagesArray.map((imgItem) => {
+        const createImageDiv = document.createElement('div')
+        const createImage = new Image()
 
-    const displayImagesFromApi = (imagesArray) => {
-        const imageContainer = document.querySelector('.image-container')
+        createImage.src = imgItem.links[0].href
+        createImageDiv.appendChild(createImage)
+        return imageContainer.appendChild(createImageDiv)
+        })
+}
 
-        const displayImages = imagesArray.map((imgItem) => {
-            const createImageDiv = document.createElement('div')
-            const createImage = new Image()
-
-            createImage.src = imgItem.links[0].href
-            createImageDiv.appendChild(createImage)
-            return imageContainer.appendChild(createImageDiv)
-            })
-    }
+const displayPastSavedSearches = (searchValue) => {
+    localWindowStorage.setItem(`searchTerm`, searchValue)
+    const getSavedSearches = localWindowStorage.getItem(`searchTerm`)
+    showPreviousSearches.innerHTML += `${getSavedSearches} `
+}
